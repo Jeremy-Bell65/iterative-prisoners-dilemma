8b9901bd3b29682bd667aa6787e19afd51e25c28
Jeremy-Bell65 <60659982+Jeremy-Bell65@users.noreply.github.com>
2020-02-24 08:45:09 -0600

    Update example_class[1].py

--- example_class[1].py
+++ example_class[1].py
@@ -1,14 +1,34 @@
-class Student(object):
-    def __init__(self, height=65, weight=130):
-        self.height = height
-        self.weight = weight
-    def grow(self, change=1):
-        self.height += change
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
 
-'''The following code can be executed in the IPython session:
-In[]:   alex = Student(height=72)   # Creates a Student object called alex with a height of 72.
-In[]:   print alex.weight           # Prints the weight attribute of the alex object.
-Out[]:  130                         # The output will be 130 becuase that is the default weight and was not changed.
-In[]:   alex.grow(2)                # Calls the grow method on the object alex
-In[]:   print alex.height           # Prints the current height of the alex object.
-'''
+team_name = 'theBosses' # Only 10 chars displayed.
+strategy_name = 'Never Snitch Unless Snitched Upon'
+strategy_description = 'We Do Not Snitch Unless They Have Previously Snitched'
+    
+def move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] and their_history[-1].
+    
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
+    
+    if len(my_history)==0: 
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' 
+    else:
+        return 'c'
